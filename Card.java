68f6730b941455a01a3b06e48f84377fcb67aa4a
Kent <kkawahara1028@gmail.com>
2015-01-20 15:16:22 -0800

    Finished Card Class

--- Card.java
+++ Card.java
@@ -1,23 +1,34 @@
 public class Card implements Cloneable{
-  protected String value;
+    protected String suit, faces[] = {"Jack", "Queen", "King", "Ace"};
+    protected int value;
   
-  public Card(){
-    value = "";
-  }
+    public Card(){
+        value = "";
+    }
   
-  public Card(String s){
-    value = s;
-  }
+    public Card(String s){
+        value = s;
+    }
   
-  public void setValue(String s){
-    value = s;
-  }
+    public void setValue(String s){
+        value = s;
+    }
   
-  public Card clone(){
-    return new Card(value);
-  }
+    public Card clone(){
+        return new Card(value);
+    }
   
-  public String toString(){
-    return value;
-  }
+    public String toString(){
+        String s = "";
+        if(value > 10){
+            for(itn i = 0; i < 5; i++){
+                if(i == value - 11){
+                    s = faces[i];
+                }
+            }
+        }else{
+            s += value
+        }
+        return s + " of " + suit;
+    }
 }
