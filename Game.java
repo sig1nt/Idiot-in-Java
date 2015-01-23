98189c32b375c15b9e7656fc8c18141cd76ebff0
Max <mzinkus@calpoly.edu>
2015-01-23 11:59:39 -0800

    Update Game, add new methods and develop main

--- Game.java
+++ Game.java
@@ -1,33 +1,45 @@
 public class Game {
 	
+	// Constructor, builds a Deck deck, a Pile pile, and an ArrayList<Player> players
 	public Game() {
 		Deck deck = new Deck();
 		Pile pile = new Pile();
-		Player[Player.numPlayers] players;
-		for (int i=0; i < Player.numHuman; i++) {
-			players[i] = new Player(true, getName(i), deck);
+		int humanCount = getPlayerCount(true);
+		int cpuCount = getPlayerCount(false);
+		Player[humanCount+cpuCount] playersArray;
+		for (int i=0; i < humanCount; i++) {
+			playersArray[i] = new Player(true, getPlayerName(i), deck);
 		}
-		for (int i=0; i < Player.numCPUs; i++) {
-			players[i] = new Player(false, "CPU-" + i, deck);
+		for (int i=0; i < cpuCount; i++) {
+			playersArray[i] = new Player(false, "CPU-" + i, deck);
 		}
+		ArrayList<Player> players = new ArrayList().addAll(playersArray);
     }
 
-	private String getName(int i) {
+	// Determines how many players of each type in Human, CPU to instantiate
+	private int getPlayerCount(boolean isHuman) {
+		if (isHuman) {
+		    // TODO Console out "How many human players? "
+			// return <int> Console in
+		} else {
+			// TODO Console out "How many cpu players? "
+			// return <int> Console in
+		}
+		return 1;
+
+	// Gets names for human players
+	private String getPlayerName(int i) {
 		// TODO
 		// Console out "Please input your name: "
 		// name = Console in
 		// Eventually, make it parameterless
 		return "Test-" + i;
 
-	/* Takes the list of players with sorted hands and determines who goes first.
-	 * @param Player[]
-	 * @return Player
-	 */
-	private Player goesFirst(Player[] players) {
+	// Takes the list of players with sorted hands and determines who goes first.
+	private Player goesFirst(ArrayList<Player> players) {
 		Card lowCard = players[0].hand[0];
 		Player goesFirst;
-		String[] suits_array = {"Clubs", "Diamonds", "Spades", "Hearts"};
-		List<String> suits = Arrays.asList(suits_array); // Is this how I arrayList?
+		ArrayList<String> suits = new ArrayList().addAll(Arrays.asList("Clubs", "Diamonds", "Spades", "Hearts"));
 		for (Player p : players) {
 				c = p.hand[0];
 				if (c.value <= lowCard.value) {
@@ -46,15 +58,42 @@ public class Game {
 		return goesFirst;
 	}
 
+	// Lets player iteration wrap around the ArrayList<Player> players
+	private int nextPlayer(int i, int numPlayers, boolean goAgain) {
+		if (goAgain) {
+			return i;
+		} else if ( i < numPlayers ) {
+			return i++;
+		} else {
+			return 0;
+		}
+	}
+
 	/* Loop that executes the actual game.
 	 * Iterates through players and 
 	 * finishes when one has no hand and no fd
 	 */
     public static void main() {
 		boolean gameOver = false;
-		// TODO determine who goes first, and resulting play order clockwise
+		int indexFirst = Game.players.indexOf(goesFirst(Game.players));
+		int i;
+		int moveExitStatus;
+		boolean goAgain = false;
 		while (not gameOver) {
-			// TODO
+			if (moveExitStatus == 2) {
+				winGame(players[i]);
+			} else if (moveExitStatus == 1) {
+				i--;
+			}
+			for (i=indexFirst; i < Player.numPlayers; i=nextPlayer(i, Player.numPlayers, goAgain)) {
+				goAgain = false;
+				moveExitStatus = players[i].move(Game.pile, Game.deck);
+				if (moveExitStatus == 2) {
+					break;
+				} else if (moveExitStatus == 1) {
+					goAgain = true;
+				}
+			}
 		}
 	}
 }
