0946e692dcde47a68547ac577063fb22ed2ce3fa
Nick Gonella <ngonella@calpoly.edu>
2015-03-02 11:44:33 -0800

    Here are some processing files.

---
+++ pTest.java
@@ -0,0 +1,15 @@
+import processing.core.*;
+
+public class pTest extends PApplet{
+   public static void main(String[] args)  {
+      PApplet.main(new String[] { "--present", "pTest" });
+   }
+
+   public void setup(){
+      size(400,400);
+      
+   }
+
+   public void draw(){
+   }
+}
