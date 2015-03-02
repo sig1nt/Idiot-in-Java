65ed40369650051cb796eed366029fbf1043e051
thewells1024 <kkawahara1028@gmail.com>
2015-03-02 11:54:38 -0800

    Added Runnable class to pTest

--- pTest.java
+++ pTest.java
@@ -1,15 +1,21 @@
 import processing.core.*;
 
 public class pTest extends PApplet{
-   public static void main(String[] args)  {
-      PApplet.main(new String[] { "--present", "pTest" });
-   }
-
-   public void setup(){
-      size(400,400);
-      
-   }
-
-   public void draw(){
-   }
+    public static void main(String[] args)  {
+        PApplet.main(new String[] { "--present", "pTest" });
+    }
+    
+    public void setup(){
+        size(400,400);
+        new Thread(new GameLogic()).run();
+    }
+    
+    public void draw(){
+    }
+    
+    class GameLogic implements Runnable{
+        void run(){
+            new Game().play();
+        }
+    }
 }
