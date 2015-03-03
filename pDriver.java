c457d5eab2ce008acc193c7004e273edfb069ffa
Nick Gonella <ngonella@calpoly.edu>
2015-03-03 11:09:09 -0800

    Added start of animations.

--- pDriver.java
+++
@@ -1,6 +0,0 @@
-public class pDriver{
-   public static void main(String[] args){
-      pTest driver = new pTest();
-      driver.setup();
-   }
-}
--- pTest.java
+++ pTest.java
@@ -1,20 +1,41 @@
 import processing.core.*;
 
 public class pTest extends PApplet{
+
+    int onSelect = 0;
+
     public static void main(String[] args)  {
+        new Thread(new GameLogic()).start();
         PApplet.main(new String[] { "--present", "pTest" });
     }
     
     public void setup(){
-        size(400,400);
-        new Thread(new GameLogic()).run();
+        noStroke();
+        size(800,800);
+        background(25,77,30);
+        fill(250,164,43);
+        rect(25,25,750,725);
     }
     
     public void draw(){
+        fill(25,77,30);
+        rect(50,50,700,675);
+        onSelect = (mouseX>100 && mouseX < 250 && mouseY >525 && mouseY < 725)?-20:0;
+        stroke(1);
+        fill(255);
+	      rect(100,525+onSelect,150,200);
+        rect(550,525,150,200);
+        rect(325,525,150,200);
+
+        rect(100,50,150,200);
+        rect(550,50,150,200);
+        rect(325,50,150,200);
+
+        rect(300,300,200,150);
     }
     
-    class GameLogic implements Runnable{
-        void run(){
+    static class GameLogic implements Runnable{
+        public void run(){
             new Game().play();
         }
     }
