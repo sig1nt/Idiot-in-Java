a1ca3244d72a4e15b338fcd11089b6a2472a0125
Max <mzinkus@calpoly.edu>
2015-03-05 20:10:50 -0800

    Define and implement ControllerInitializeIPC message

--- ControllerInitializeIPC.java
+++ ControllerInitializeIPC.java
@@ -4,8 +4,32 @@ public class ControllerInitializeIPC extends GenericIPC {
     public static void main(String[] args) {
     }
 
-    public ControllerInitializeIPC(ArrayList<Player> humans, ArrayList<Player> cpus) {
+    public ControllerInitializeIPC(ArrayList<Player> humans, ArrayList<Player> cpus, ArrayList<Card> hand, ArrayList<Card> faceups) {
         message = "init:";
-        // TODO
+        message += (String) cpus.length();
+        message += "|";
+        for (cpu : cpus) {
+            message += "|"
+            for (card : cpu.faceup) {
+                message += card.getShortName() + "|";
+            }
+        }
+        message += "|";
+        message += (String) humans.length();
+        message += "|";
+        for (human : humans) {
+            message += "|";
+            for (card : human.faceup) {
+                message += card.getShortName() + "|";
+            }
+        }
+        message += "|";
+        for (card : hand) {
+            message += card.getShortName() + "|";
+        }
+        for (card : faceups) {
+            message += card.getShortName() + "|";
+        }
+        message += "|";
     }
 }
--- GenericIPC.java
+++ GenericIPC.java
@@ -25,5 +25,7 @@ public class GenericIPC {
 // RequestPlayFromHand: "play:hand"
 // RequestPlayFromFaceups: "play:faceups"
 // PlaysFacedown: "play:facedown:|card.getShortName()|" e.g. "play:facedown:|10c|"
-// GameOver: "winner" + "player.name" e.g. "winner:Max"
-// Initialize: "init:" + TODO
+// GameOver: "winner:" + "player.name" e.g. "winner:Max"
+// Initialize: "init:" + "cpuCount|" + "|cpuXfaceup1|cpuXfaceup2|cpuXfaceup3|" for X in range(cpuCount) + "|humanCount|" + "|humanXfaceup1|humanXfaceup2|humanXfaceup3|" for X in range(humanCount)
+//  + "||hand1|hand2|hand3|faceup1|faceup2|faceup3||"
+//  e.g. "init:2||3s|2h|10c||5d|7c|14h||3||6c|6s|12d||4h|5s|11h||2c|3c|13h||7d|8h|9s|8c|10s|11s||"
