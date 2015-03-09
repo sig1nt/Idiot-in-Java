70a825e4c047d3395e8bc877d9bb2c861f73131a
Nick Gonella <ngonella@calpoly.edu>
2015-03-09 10:55:38 -0700

    Furthered the animations.

--- pTest.java
+++ pTest.java
@@ -4,14 +4,22 @@ public class pTest extends PApplet{
 
     int onSelect = 0;
     Game daGame;
+    DrawableCard[] myCards = new DrawableCard[3];
+    DrawableCard[] yourCards = new DrawableCard[3];
     
     public static void main(String[] args)  {
         PApplet.main(new String[] { "--present", "pTest" });
     }
     
     public void setup(){
-        daGame = new Game((PApplet)this);
-        new Thread(new GameLogic()).start();
+        //daGame = new Game((PApplet)this);
+        //new Thread(new GameLogic()).start();
+        for(int i = 0; i < 3; i++){
+            myCards[i]= new DrawableCard(this,100+(225*i),525,"Spades",12,true);
+        }
+        for(int i = 0; i < 3; i++){
+            yourCards[i]= new DrawableCard(this,100+(225*i),50,"Spades",12,false);
+        }
         noStroke();
         size(800,800);
         background(25,77,30);
@@ -25,20 +33,21 @@ public class pTest extends PApplet{
         onSelect = (mouseX>100 && mouseX < 250 && mouseY >525 && mouseY < 725)?-20:0;
         stroke(1);
         fill(255);
-	    rect(100,525+onSelect,150,200);
-        rect(550,525,150,200);
-        rect(325,525,150,200);
 
-        rect(100,50,150,200);
-        rect(550,50,150,200);
-        rect(325,50,150,200);
+        for(DrawableCard c : myCards){
+            c.draw();
+        }
+
+        for(DrawableCard c : yourCards){
+            c.draw();
+        }
 
         rect(300,300,200,150);
     }
     
-    class GameLogic implements Runnable{
+    /*class GameLogic implements Runnable{
         public void run(){
             daGame.play();
         }
-    }
+    }*/
 }
